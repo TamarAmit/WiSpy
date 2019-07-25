101137d472b5b10ac9cfedbb74bc571f599ec2a6
ghqpio <sergey.rura@elbitsystems.com>
2019-07-25 07:20:51 +0300

    WS Integration

--- src/components/mainBuilding/mainBuilding.jsx
+++ src/components/mainBuilding/mainBuilding.jsx
@@ -2,61 +2,58 @@ import React, {Component} from 'react';
 import './mainBuilding.css'
 import io from 'socket.io-client';
 
-const socket = io('http://10.20.20.114:3030');
-
-socket.on('message', (data) => {
-	console.log(data.msg)
-});
-
-socket.on('connect_error', (error) => {
-	console.log(error)
-});
-
-socket.on('error', (error) => {
-	console.log(error);
-});
+const socket = io('http://10.20.20.102:3030');
 
 const WifiTransmitterImage = ({ top, left }) => {
 	return (
 			<div className={`sensor`} style={{top, left}}/>
 	)
-}
+};
 
 class MainBuilding extends Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {manInTheRoomStatus: "unvisible", score: 0};
-		this.myVar = "a";
+		this.state = {
+			isVisible: false,
+			score: 0
+		};
 	}
 
 	componentDidMount() {
 		// this.timerID = setInterval(() => this.checkManInTheRoomStatus(), 1000);
-		this.checkManInTheRoomStatus();
+		// this.checkManInTheRoomStatus();
+
+		socket.on('message', (data) => {
+			console.log(data);
+			const score = parseInt(data.score);
+
+			console.log(score);
+			this.setState({
+				isVisible: score >= 50,
+				score: parseInt(data.score),
+				timestamp: data.timestamp
+			})
+		});
+
+		socket.on('connect_error', (error) => {
+			console.log(error)
+		});
+
+		socket.on('error', (error) => {
+			console.log(error);
+		});
 	}
 
-	checkManInTheRoomStatus() {
-		if ("a" === this.myVar) {
-			this.setState({
-				manInTheRoomStatus: "visible",
-				score: 60
-			});
-
-			this.myVar = "b";
-		} else {
-			this.setState({
-				manInTheRoomStatus: "unvisible",
-				score: 60
-			});
-
-			this.myVar = "a"
-		}
+	componentWillUnmount() {
+		socket.disconnect();
 	}
 
 	render() {
 		let indications;
-		let scoreClass
-		if (this.state.manInTheRoomStatus === "visible") {
+		let scoreClass;
+		console.log(this.state);
+		if (this.state.isVisible) {
 			indications = "show";
 			scoreClass = this.state.score > 50 ? "green" : "yellow";
 		} else {
